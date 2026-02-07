5ea195414ea6830b0a397372fb8fa9216b7db953
Annkit <annkit461@gmail.com>
2026-02-07 12:44:14 +0530

    introduction to RecursionPart1

--- src/RecursionPart1.java
+++ src/RecursionPart1.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class RecursionPart1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        main(n);
+
+
+    }
+    // When a function call itself until a condition is met is called Recursion
+
+    static void main(int n){
+
+        while(n < 1000000000){
+            n++;
+            main(n);
+        }
+
+    }
+
 }
